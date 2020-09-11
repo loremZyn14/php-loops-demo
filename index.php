65b4751d3aeb1991de2a3be4264e105a84d93ee3
LoremZyn14 <yasuaimo@gmail.com>
2020-09-11 08:11:18 +0800

    Create demo code for interview

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+//WHILE LOOP
+
+// $number = 0;
+// while($number < 10){
+//     echo $number;
+//     $number++;
+// }
+//DO -WHILE LOOP
+// do {
+//     echo $number;
+//     $number++;
+// } while ($number < 10);
+// FOR LOOP
+// for ($i; $i < 10 ; $i++) { 
+//     echo $i;
+// }
+// FOREACH LOOP
+// normal foreach
+// $colors = array("red","green", "blue","yellow");
+// foreach($colors as $color){
+//     echo $color . "<br>";
+// }
+
+// with key foreach
+
+// $colors2 = array(1=> "red",2=>"green",3=>"blue",4=>"yellow");
+// foreach($colors2 as $colorNumber => $color){
+//     echo $colorNumber . " " . $color . "<br>";
+// }
+?>
